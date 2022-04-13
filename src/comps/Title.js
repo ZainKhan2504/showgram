b1a6d9df245bb0dbd9fa4388d66268646e57aedf
zaikh847 <zaikh847@student.liu.se>
2022-04-13 17:27:51 +0500

    Setup starter files

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>ShowGram</h1>
+      <h2>Show Yourself To The World</h2>
+      <p>Share pictures of your choice with love ones</p>
+    </div>
+  );
+};
+
+export default Title;
